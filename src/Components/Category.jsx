f2462241e369e01a77885d87d71ad48597ed1578
Surbhi Pandey <114894985+surbhi-973@users.noreply.github.com>
2023-06-28 00:30:48 +0530

    Update Category.jsx

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -29,7 +29,7 @@ function Category() {
 const List = styled.div`
     display: flex;
     justify-content: center;
-    margin: 2rem 0rem;
+    margin: 1rem 0rem;
 `;
 
 const SLink = styled(NavLink)`
@@ -38,22 +38,22 @@ const SLink = styled(NavLink)`
     justify-content: center;
     align-items: center;
     border-radius: 50%;
-    margin-right: 2rem;
+    margin-right: 1rem;
     text-decoration: none;
     background: linear-gradient(35deg, #494949, #313131);
-    width: 6rem;
-    height: 6rem;
+    width: 3.5rem;
+    height: 3.5rem;
     cursor: pointer;
     transform: scale(0.8);
 
     h4{
         color: white;
-        font-size: 0.8rem;
+        font-size: 0.6rem;
     }
 
     svg{
         color: white;
-        font-size: 1.5rem;
+        font-size: 0.95rem;
     }
     &.active{
         background: linear-gradient(to right, #f27121, #e94057);
